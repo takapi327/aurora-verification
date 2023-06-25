d906202520a153978e53e4ee4e1eca6a8d1ec63d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-25 14:25:33 +0900

    Fix DatabaseConnection

--- app/interfaceAdapter/controllers/DatabaseConnection.scala
+++ app/interfaceAdapter/controllers/DatabaseConnection.scala
@@ -1,9 +1,11 @@
 package interfaceAdapter.controllers
 
 import java.security.Security
+import java.sql.ResultSet
 import javax.inject.Inject
 
 import scala.util.Random
+import scala.annotation.tailrec
 
 import com.zaxxer.hikari.HikariDataSource
 
@@ -27,15 +29,23 @@ class DatabaseConnectionController @Inject()(implicit
       insert.executeUpdate()
       val statement = connection.prepareStatement("SELECT name FROM user")
       val resultSet = statement.executeQuery()
-      while (resultSet.next()) {
-        println(resultSet.getString("name"))
-      }
+      val users = getResult(resultSet)
       insert.close()
       statement.close()
       connection.close()
-      Ok("ok")
+      Ok(users.mkString("\n"))
     } else {
       NotFound("コネクションが存在しない")
+    }
+  }
+
+  @tailrec()
+  private def getResult(resultSet: ResultSet, list: List[String] = Nil): List[String] = {
+    if (resultSet.next()) {
+      val value = resultSet.getString("name")
+      getResult(resultSet, value :: list)
+    } else {
+      list
     }
   }
 }
